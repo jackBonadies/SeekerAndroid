21fb0ac4e09a59fc3fa15d1e82fe3fe72e6faa98
xarantolus <xarantolus@protonmail.com>
2021-10-11 08:21:26 +0200

    GitHub Actions: group output

--- metascoop/main.go
+++ metascoop/main.go
@@ -64,79 +64,82 @@ func main() {
 	var apkInfoMap = make(map[string]apps.AppInfo)
 
 	for _, app := range appsList {
-		log.SetPrefix(fmt.Sprintf("%s/%s", app.Author(), app.Name()))
+		fmt.Printf("::group::App: %s/%s\n", app.Author(), app.Name())
 
-		repo, err := apps.RepoInfo(app.GitURL)
-		if err != nil {
-			log.Printf("Error while getting repo info from URL %q: %s", app.GitURL, err.Error())
-			haveError = true
-			continue
-		}
+		func() {
+			defer fmt.Println("::endgroup::")
 
-		gitHubRepo, _, err := githubClient.Repositories.Get(context.Background(), repo.Author, repo.Name)
-		if err == nil {
-			app.Summary = gitHubRepo.GetDescription()
-
-			if gitHubRepo.License != nil && gitHubRepo.License.SPDXID != nil {
-				app.License = *gitHubRepo.License.SPDXID
+			repo, err := apps.RepoInfo(app.GitURL)
+			if err != nil {
+				log.Printf("Error while getting repo info from URL %q: %s", app.GitURL, err.Error())
+				haveError = true
+				return
 			}
-		}
 
-		releases, err := apps.ListAllReleases(githubClient, repo.Author, repo.Name)
-		if err != nil {
-			log.Printf("Error while listing repo releases for %q: %s\n", app.GitURL, err.Error())
-			haveError = true
-			continue
-		}
+			gitHubRepo, _, err := githubClient.Repositories.Get(context.Background(), repo.Author, repo.Name)
+			if err == nil {
+				app.Summary = gitHubRepo.GetDescription()
 
-		for _, release := range releases {
-			if release.Prerelease != nil && *release.Prerelease {
-				if release.TagName == nil {
-					nt := "nil-tagname"
-					release.TagName = &nt
+				if gitHubRepo.License != nil && gitHubRepo.License.SPDXID != nil {
+					app.License = *gitHubRepo.License.SPDXID
 				}
-				log.Printf("Skipping prerelease %q", *release.TagName)
-				continue
 			}
 
-			apk := apps.FindAPKRelease(release)
-			if apk == nil {
-				continue
-			}
-
-			appName := apps.GenerateReleaseFilename(app.Name(), *release.TagName)
-
-			apkInfoMap[appName] = app
-
-			appTargetPath := filepath.Join(*repoDir, appName)
-
-			// If the app file already exists for this version, we continue
-			if _, err := os.Stat(appTargetPath); !errors.Is(err, os.ErrNotExist) {
-				log.Printf("Already have version %q at %q", *release.TagName, appTargetPath)
-				continue
-			}
-
-			dlCtx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
-			defer cancel()
-
-			appStream, _, err := githubClient.Repositories.DownloadReleaseAsset(dlCtx, repo.Author, repo.Name, *apk.ID, http.DefaultClient)
+			releases, err := apps.ListAllReleases(githubClient, repo.Author, repo.Name)
 			if err != nil {
-				log.Printf("Error while downloading app %q (artifact id %d) from from release %q: %s", app.GitURL, *apk.ID, *release.TagName, err.Error())
+				log.Printf("Error while listing repo releases for %q: %s\n", app.GitURL, err.Error())
 				haveError = true
-				continue
+				return
 			}
 
-			err = downloadStream(appTargetPath, appStream)
-			if err != nil {
-				log.Printf("Error while downloading app %q (artifact id %d) from from release %q to %q: %s", app.GitURL, *apk.ID, *release.TagName, appTargetPath, err.Error())
-				haveError = true
-				continue
-			}
+			for _, release := range releases {
+				if release.Prerelease != nil && *release.Prerelease {
+					if release.TagName == nil {
+						nt := "nil-tagname"
+						release.TagName = &nt
+					}
+					log.Printf("Skipping prerelease %q", *release.TagName)
+					continue
+				}
 
-			log.Printf("Successfully downloaded %q", appTargetPath)
-		}
+				apk := apps.FindAPKRelease(release)
+				if apk == nil {
+					continue
+				}
+
+				appName := apps.GenerateReleaseFilename(app.Name(), *release.TagName)
+
+				apkInfoMap[appName] = app
+
+				appTargetPath := filepath.Join(*repoDir, appName)
+
+				// If the app file already exists for this version, we continue
+				if _, err := os.Stat(appTargetPath); !errors.Is(err, os.ErrNotExist) {
+					log.Printf("Already have version %q at %q", *release.TagName, appTargetPath)
+					continue
+				}
+
+				dlCtx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+				defer cancel()
+
+				appStream, _, err := githubClient.Repositories.DownloadReleaseAsset(dlCtx, repo.Author, repo.Name, *apk.ID, http.DefaultClient)
+				if err != nil {
+					log.Printf("Error while downloading app %q (artifact id %d) from from release %q: %s", app.GitURL, *apk.ID, *release.TagName, err.Error())
+					haveError = true
+					continue
+				}
+
+				err = downloadStream(appTargetPath, appStream)
+				if err != nil {
+					log.Printf("Error while downloading app %q (artifact id %d) from from release %q to %q: %s", app.GitURL, *apk.ID, *release.TagName, appTargetPath, err.Error())
+					haveError = true
+					continue
+				}
+
+				log.Printf("Successfully downloaded %q", appTargetPath)
+			}
+		}()
 	}
-	log.SetPrefix("")
 
 	if !*debugMode {
 		// Now, we run the fdroid update command
