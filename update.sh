5c111dafae32a1d202e959aa3a6dfdd4752d4ea8
xarantolus <xarantolus@protonmail.com>
2021-10-11 08:35:27 +0200

    Better logging

--- update.sh
+++ update.sh
@@ -15,9 +15,12 @@ set -e
 
 if [ $EXIT_CODE -eq 2 ]; then
     # Exit code 2 means that there were no significant changes
+    echo "This means that there were no significant changes"
     exit 0
 elif [ $EXIT_CODE -eq 0 ]; then
     # Exit code 0 means that we can commit everything & push
+
+    echo "This means that we now have changes we should push"
 
     git config --global user.name 'github-actions'
     git config --global user.email '41898282+github-actions[bot]@users.noreply.github.com'
@@ -26,5 +29,7 @@ elif [ $EXIT_CODE -eq 0 ]; then
     git commit -m"Automated update"
     git push
 else 
+    echo "This is an unexpected error"
+
     exit $EXIT_CODE
 fi
