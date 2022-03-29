4d998b731bcf778e5c040bf6871f5a4f5b855a6b
jackBonadies <jbonadies6@gmail.com>
2022-03-29 07:14:39 -0400

    2.9.5
    Former-commit-id: f7b81d2347a83210e2dc64aca2ae8543f167f88b

--- AndriodApp1/MainActivity.cs
+++ AndriodApp1/MainActivity.cs
@@ -2659,21 +2659,29 @@ namespace AndriodApp1
         /// <returns>true if changed</returns>
         public static bool SetNetworkState(Context context)
         {
-            ConnectivityManager cm = (ConnectivityManager)context.GetSystemService(Context.ConnectivityService);
-
-            if(cm == null) //null if class is not a supported system service.
+            try
             {
+                ConnectivityManager cm = (ConnectivityManager)context.GetSystemService(Context.ConnectivityService);
+
+                if(cm == null) //null if class is not a supported system service.
+                {
+                    return false;
+                }
+
+                if (cm.ActiveNetworkInfo != null && cm.ActiveNetworkInfo.IsConnected)
+                {
+                    bool oldState = SoulSeekState.CurrentConnectionIsUnmetered;
+                    SoulSeekState.CurrentConnectionIsUnmetered = IsUnmetered(context, cm);
+                    MainActivity.LogDebug("SetNetworkState is metered " + !SoulSeekState.CurrentConnectionIsUnmetered);
+                    return oldState != SoulSeekState.CurrentConnectionIsUnmetered;
+                }
                 return false;
             }
-
-            if (cm.ActiveNetworkInfo != null && cm.ActiveNetworkInfo.IsConnected)
+            catch(Exception e)
             {
-                bool oldState = SoulSeekState.CurrentConnectionIsUnmetered;
-                SoulSeekState.CurrentConnectionIsUnmetered = IsUnmetered(context, cm);
-                MainActivity.LogDebug("SetNetworkState is metered " + !SoulSeekState.CurrentConnectionIsUnmetered);
-                return oldState != SoulSeekState.CurrentConnectionIsUnmetered;
+                MainActivity.LogFirebase("SetNetworkState" + e.Message + e.StackTrace);
+                return false;
             }
-            return false;
         }
 
         private static bool IsUnmetered(Context context, ConnectivityManager cm)
