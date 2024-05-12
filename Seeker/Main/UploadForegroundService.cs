b8bbb2a1a9af7a667ecb128eda99da4f11452b61
jackBonadies <jbonadies6@gmail.com>
2024-05-12 17:09:19 -0400

    Target Android 14.  ForegroundServiceTypes.

--- Seeker/Main/UploadForegroundService.cs
+++ Seeker/Main/UploadForegroundService.cs
@@ -11,9 +11,8 @@ using System.Text;
 
 namespace Seeker
 {
-    // TODOORG seperate class
     //Services are natural singletons. There will be 0 or 1 instance of your service at any given time.
-    [Service(Name = "com.companyname.andriodapp1.UploadService")]
+    [Service(Name = "com.companyname.andriodapp1.UploadService", ForegroundServiceType = Android.Content.PM.ForegroundService.TypeDataSync)]
     public class UploadForegroundService : Service
     {
         public const int NOTIF_ID = 1112;
