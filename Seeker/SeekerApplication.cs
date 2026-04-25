1f936916f2c6aa2195b329b184071f9ce2e894c4
jackBonadies <jbonadies6@gmail.com>
2026-04-25 16:34:13 -0400

    add debug mock config

--- Seeker/SeekerApplication.cs
+++ Seeker/SeekerApplication.cs
@@ -247,21 +247,23 @@ namespace Seeker
             //need search response and enqueue download action...
             //SeekerState.SoulseekClient = new SoulseekClient(new SoulseekClientOptions(messageTimeout: 30000, enableListener: false, autoAcknowledgePrivateMessages: false, acceptPrivateRoomInvitations:PreferencesState.AllowPrivateRoomInvitations)); //Enable Listener is False.  Default is True.
             #if DEBUG
-            SeekerState.SoulseekClient = new MockSoulseekClient();
-            SharingService.TurnOnSharing();
-            //128,
-            //new SoulseekClientOptions(
-            //    minimumDiagnosticLevel: LOG_DIAGNOSTICS ? Soulseek.Diagnostics.DiagnosticLevel.Debug : Soulseek.Diagnostics.DiagnosticLevel.Info,
-            //    messageTimeout: 30000,
-            //    enableListener: PreferencesState.ListenerEnabled,
-            //    autoAcknowledgePrivateMessages: false,
-            //    acceptPrivateRoomInvitations: PreferencesState.AllowPrivateRoomInvitations,
-            //    listenPort: PreferencesState.ListenerPort,
-            //    maximumConcurrentDownloads: PreferencesState.LimitSimultaneousDownloads ? PreferencesState.MaxSimultaneousLimit : int.MaxValue,
-            //    maximumConcurrentSearches: 5,
-            //    serverConnectionOptions: ServerConnectionOptionsWithKeepAlive,
-            //    addressResolver: ResolveAddressAsync,
-            //    userInfoResolver: UserInfoResponseHandler));
+                #if MOCK
+                SeekerState.SoulseekClient = new MockSoulseekClient();
+                SharingService.TurnOnSharing();
+                #else
+                new SoulseekClientOptions(
+                    minimumDiagnosticLevel: LOG_DIAGNOSTICS ? Soulseek.Diagnostics.DiagnosticLevel.Debug : Soulseek.Diagnostics.DiagnosticLevel.Info,
+                    messageTimeout: 30000,
+                    enableListener: PreferencesState.ListenerEnabled,
+                    autoAcknowledgePrivateMessages: false,
+                    acceptPrivateRoomInvitations: PreferencesState.AllowPrivateRoomInvitations,
+                    listenPort: PreferencesState.ListenerPort,
+                    maximumConcurrentDownloads: PreferencesState.LimitSimultaneousDownloads ? PreferencesState.MaxSimultaneousLimit : int.MaxValue,
+                    maximumConcurrentSearches: 5,
+                    serverConnectionOptions: ServerConnectionOptionsWithKeepAlive,
+                    addressResolver: ResolveAddressAsync,
+                    userInfoResolver: UserInfoResponseHandler);
+                #endif
             #else
             SeekerState.SoulseekClient = new SoulseekClient(
                 128,
