c3203689a47d07f68f0c43fd31e8791f8df77138
jackBonadies <jbonadies6@gmail.com>
2026-08-28 22:41:52 -0400

    remove vendored Soulseek.NET copy, to be replaced by git subtree

--- Seeker/Soulseek.NET-master/examples/Web/web/src/lib/search.js
+++
@@ -1,9 +0,0 @@
-import api from './api';
-
-export const search = ({ id, searchText }) => {
-  return api.post(`/searches`, { id, searchText });
-};
-
-export const getStatus = async ({ id }) => {
-  return (await api.get(`/searches/${encodeURIComponent(id)}`)).data;
-};
